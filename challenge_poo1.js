5add9bd742c8d40f038f8826f4faf8802e3bafcf
Albja <alban.jaunet@sncf.fr>
2023-03-27 15:51:07 +0200

    correction

--- challenge_poo1.js
+++ challenge_poo1.js
@@ -4,16 +4,16 @@ var Person = /** @class */ (function () {
         this.age = age;
     }
     Person.prototype.tellMyName = function () {
-        return this.name;
+        console.log("I am ".concat(this.name));
     };
     Person.prototype.tellMyAge = function () {
-        return this.age;
-    };
-    Person.prototype.tell = function () {
-        console.log("I am ".concat(this.name, " and I am ").concat(this.age));
+        console.log("I am ".concat(this.age, " years old"));
     };
     return Person;
 }());
 var Person1 = new Person('John', 40);
 var Person2 = new Person('Mary', 35);
-//I am <name> et I am <age> years old  "John" et 40, "Mary" et 35,
+Person1.tellMyName();
+Person1.tellMyAge();
+Person2.tellMyName();
+Person2.tellMyAge();
